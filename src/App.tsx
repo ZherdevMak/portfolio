ca63c61ab26a61cf2f5e20be4f26ec381542490e
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-18 16:54:08 +0300

    In progress

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,6 @@ import React from 'react';
 import './App.css';
 import Header from "./Header/Header";
 import Main from "./Main/Main";
-import Skills from "./Skills/Skills";
-import Works from "./Works/Works";
-import Contacts from "./Contacts/Contacts";
 import Footer from "./Footer/Footer";
 
 function App() {
@@ -12,9 +9,6 @@ function App() {
         <div className="App">
             <Header/>
             <Main/>
-            <Skills/>
-            <Works/>
-            <Contacts/>
             <Footer/>
         </div>
     );
--- src/Main/Main.tsx
+++ src/Main/Main.tsx
@@ -1,20 +1,30 @@
 import React from 'react';
 import stl from './Main.module.css'
+import Skills from "../Skills/Skills";
+import Works from "../Works/Works";
+import Contacts from "../Contacts/Contacts";
+
 const Main = () => {
     return (
-        <div className={stl.wrapper}>
-          <div className={stl.container}>
-              <div className={stl.text}>
-                  <div >Hi,there!</div>
-                  <h1>I am Zherdev Maxim</h1>
-                  <div>a Web developer</div>
-              </div>
-              <div className={stl.photo}>
+        <div>
+            <div className={stl.wrapper}>
+                <div className={stl.container}>
+                    <div className={stl.text}>
+                        <div>Hi,there!</div>
+                        <h1>I am Zherdev Maxim</h1>
+                        <div>a Web developer</div>
+                    </div>
+                    <div className={stl.photo}>
 
-              </div>
-          </div>
+                    </div>
+                </div>
+            </div>
+            <Skills/>
+            <Works/>
+            <Contacts/>
         </div>
-    );
+    )
+        ;
 };
 
 export default Main;
