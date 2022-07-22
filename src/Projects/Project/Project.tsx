73e342fe142a5e93a5e33f638f64560a555cebc8
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-22 18:16:35 +0300

    work with img

--- src/Projects/Project/Project.tsx
+++ src/Projects/Project/Project.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import stl from "./Project.module.css";
+import image from '../../Components/Images/comp.jpg'
 
 type WorkPropsType = {
     ProjectName: string
@@ -8,10 +9,14 @@ type WorkPropsType = {
 const Project = (props: WorkPropsType) => {
     return (
         <div className={stl.container}>
-            <div className={stl.backgroundContainer}>
-                <button>Explore</button>
+            <div className={stl.imageContainer}>
+                <div className={stl.projectOverlay}></div>
+                <img className={stl.image} src={image}/>
+                <div className={stl.projectInfo}>
+                <h3 className={stl.text}>{props.ProjectName}</h3>
+                </div>
+                {/*<button>Explore</button>*/}
             </div>
-            <h3 className={stl.text}>{props.ProjectName}</h3>
             <div className={stl.description}>{props.discription}</div>
         </div>
     );
