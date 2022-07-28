6b97c503e9dae378a904d9c95a898e4530f71d16
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-28 12:35:33 +0300

    skills page stage 1

--- src/Skills/Skill/Skill.tsx
+++ src/Skills/Skill/Skill.tsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import stl from "./Skill.module.css";
+import stl from "./Skill.module.scss";
 
 type SkillPropsType = {
     skillName: string
     discription: string
+    logo:string
 }
 const Skill = (props: SkillPropsType) => {
     return (
         <div className={stl.container}>
-            <div className={stl.icon}></div>
+            <div className={stl.icon}>
+                <img className={stl.iconImage} src={props.logo} alt=""/>
+            </div>
             <div className={stl.text}>{props.skillName}</div>
             <div className={stl.description}>{props.discription}</div>
         </div>
--- src/Skills/Skills.tsx
+++ src/Skills/Skills.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import stl from "../Skills/Skills.module.css";
+import stl from "./Skills.module.scss";
 import Skill from "./Skill/Skill";
+import ReactLogo from '../Assets/Images/pngaaa.com-2508011.png'
+import nodeLogo from '../Assets/Images/nodeLogo.png'
+import cssLogo from '../Assets/Images/cssLogo.png'
 
 const Skills = () => {
     return (
         <div className={stl.wrapper}>
             <div className={stl.container}>
-                <h2 className={stl.title}>Skills</h2>
+                <h2 className={stl.title}>Skills.</h2>
                 <div className={stl.skillsContainer}>
-                    <Skill skillName={"React"}
+                    <Skill skillName={"React"} logo = {ReactLogo}
                            discription={'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minima nobis nulla numquam possimus voluptate.'}/>
-                    <Skill skillName={"NodeJS"}
+                    <Skill skillName={"NodeJS"} logo = {nodeLogo}
                            discription={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.Adipisci aliquid animi exercitationem explicabo laborum'}/>
-                    <Skill skillName={"CSS"} discription={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
+                    <Skill skillName={"CSS"} logo = {cssLogo} discription={'Lorem ipsum dolor sit amet, consectetur adipisicing elit.'}/>
                 </div>
             </div>
         </div>
