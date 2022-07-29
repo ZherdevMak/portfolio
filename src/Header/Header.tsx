520eb54b85156cdfa60a344fa857076abf4d780a
ZherdevMak <Zherdev.mak@gmail.com>
2022-07-29 11:53:07 +0300

    animated menu

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -3,8 +3,21 @@ import stl from './Header.module.scss'
 import Nav from "../Nav/Nav";
 
 const Header = () => {
+    const [scroll, setScroll] = React.useState(0);
+
+    const handleScroll = () => {
+        setScroll(window.scrollY);
+    };
+
+    const headerClass = (scroll < 350) ? stl.header : `${stl.header} + ${stl.minHeader}`
+
+    React.useEffect(() => {
+        window.addEventListener("scroll", handleScroll);
+        return () => window.removeEventListener("scroll", handleScroll);
+    }, []);
+
     return (
-        <div className={stl.header}>
+        <div className={headerClass}>
             <Nav/>
         </div>
     );
