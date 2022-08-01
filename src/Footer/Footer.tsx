af55e768ff77e6df3ccf7098633c58c471d3ac38
ZherdevMak <Zherdev.mak@gmail.com>
2022-08-01 12:03:28 +0300

    footer stage 2

--- src/Footer/Footer.tsx
+++ src/Footer/Footer.tsx
@@ -4,6 +4,9 @@ import mapMarker from '../Assets/Images/mapMarker.png'
 import mobileMarker from '../Assets/Images/mobileMarker.png'
 import wrenchMarker from '../Assets/Images/wrenchMarker.png'
 import letterMarker from '../Assets/Images/letterMarker.png'
+import facebookIcon from '../Assets/Images/faceIcon.png'
+import vkIcon from '../Assets/Images/vkIcon.png'
+import instaIcon from '../Assets/Images/instaIcon.png'
 
 const Footer = () => {
     return (
@@ -12,28 +15,33 @@ const Footer = () => {
                 {/*<h2 className={stl.title} >Zherdev Maxim</h2>*/}
                 <div className={stl.NavContainer}>
                     <div className={stl.NavElement}>
-                        <img className={stl.NavImage} src={mapMarker} alt=""/>
-                        <h2>Text</h2>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img className={stl.NavImage} src={mapMarker} alt="MapIcon"/>
+                        <h2 className={stl.NavText}>Visit my office</h2>
+                        <p className={stl.NavDiscription}>Russia, Rostov-on-Don, str.Sadovaya 44, aprt.32</p>
                     </div>
                     <div className={stl.NavElement}>
-                        <img className={stl.NavImage2} src={mobileMarker} alt=""/>
-                        <h2>Text</h2>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img className={stl.NavImage2} src={mobileMarker} alt="MobileIcon"/>
+                        <h2 className={stl.NavText}>Call me</h2>
+                        <p className={stl.NavDiscription}>+79689471982</p>
                     </div>
                     <div className={stl.NavElement}>
-                        <img className={stl.NavImage3} src={wrenchMarker} alt=""/>
-                        <h2>Text</h2>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img className={stl.NavImage3} src={wrenchMarker} alt="WrenchIcon"/>
+                        <h2 className={stl.NavText}>Work</h2>
+                        <p className={stl.NavDiscription}>Monday to friday</p>
                     </div>
                     <div className={stl.NavElement}>
-                        <img className={stl.NavImage3} src={letterMarker} alt=""/>
-                        <h2>Text</h2>
-                        <p>Lorem ipsum dolor sit amet.</p>
+                        <img className={stl.NavImage3} src={letterMarker} alt="MailIcon"/>
+                        <h2 className={stl.NavText}>Email me</h2>
+                        <p className={stl.NavDiscription}>ppmaxjer@yandex.ru</p>
                     </div>
                 </div>
                 <div className={stl.articleContainer}>
-                    <article>@2022 All rights reserved</article>
+                    <div className={stl.IconContainer}>
+                        <img className={stl.mediaIcon1} src={facebookIcon} alt=""/>
+                        <img className={stl.mediaIcon} src={vkIcon} alt=""/>
+                        <img className={stl.mediaIcon} src={instaIcon} alt=""/>
+                    </div>
+                    <article className={stl.articleText}>@2022 All rights reserved</article>
                 </div>
             </div>
         </div>
