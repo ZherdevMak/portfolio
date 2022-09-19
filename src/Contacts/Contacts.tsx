504f9e09258922b644fde0dcec70bb43a34ce6e2
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-19 21:02:26 +0300

    formik

--- src/Contacts/Contacts.tsx
+++ src/Contacts/Contacts.tsx
@@ -1,24 +1,54 @@
 import React from 'react';
 import stl from "./Contacts.module.scss";
+import {useFormik} from "formik";
 
+type FormikErrorType = {
+    name?:string,
+    email?: string,
+    subject?: string,
+    message?: string,
+}
 const Contacts = () => {
+    const formik = useFormik({
+        initialValues: {
+            name:'',
+            email: '',
+            subject: '',
+            message: '',
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {}
+            if (!values.email) {
+                errors.email = 'Required'
+            } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+                errors.email = 'Invalid email address'
+            }
+            return errors
+        },
+        onSubmit: (values) => {
+            formik.resetForm()
+        },
+    })
+
+
+
     return (
         <div id={'contacts'} className={stl.wrapper}>
             <div className={stl.container}>
                 <h2 className={stl.title}>say hello!</h2>
                 <div className={stl.formContainer}>
-                    <form className={stl.form} action="">
+                    <form className={stl.form} onSubmit={formik.handleSubmit}>
                         <input className={stl.input} type="text" placeholder={"Your name"}/>
                         <input className={stl.input} type="text" placeholder={"Your email"}/>
                         <input className={stl.input} type="text" placeholder={"Your subject"}/>
                         <textarea className={stl.textarea} placeholder={"Your message"}/>
+                        <button type={'submit'} className={stl.sendButton}>Send message</button>
                     </form>
                 </div>
-                <button className={stl.sendButton}>Send message</button>
+
 
             </div>
         </div>
     );
-};
-
+}
 export default Contacts;
