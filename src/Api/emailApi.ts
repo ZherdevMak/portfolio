13ce23ca15b0a64531a9025c616af2804ac48752
ZherdevMak <Zherdev.mak@gmail.com>
2022-09-29 13:41:49 +0300

    Contact form new end point

--- src/Api/emailApi.ts
+++ src/Api/emailApi.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 import {FormikErrorType} from "../Contacts/Contacts";
 
 export const emailSend = (values: FormikErrorType) => {
-  axios.post('http://localhost:3001/sendmail', values)
+  axios.post('https://portfolio-mailer-zherdev.herokuapp.com/sendmail', values)
     .then((res) => {
       alert('Your message has been sent! Thank You!')
       console.log(res)
